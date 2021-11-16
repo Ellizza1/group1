b01c5bbafcd85c32424a1a2dfae57705178d07af
Eliza <akmaevaeliza@gmail.com>
2021-11-16 19:38:23 +0300

    аааааа

--- Abobus/Abobus/Program.cs
+++ Abobus/Abobus/Program.cs
@@ -10,6 +10,7 @@ namespace Abobus
             Console.WriteLine("ок:C");
             Console.WriteLine("не ок ((");
             Console.WriteLine("0к");
+            Console.WriteLine(" НЕ 0К");
         }
     }
 }
