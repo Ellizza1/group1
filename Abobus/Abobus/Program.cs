252f9b0ceda2a19ddc4bc2704e7ff703f38aa5f4
romangitprogium <romangitprogium@gmail.com>
2021-11-16 19:39:09 +0300

    йцуке

--- Abobus/Abobus/Program.cs
+++ Abobus/Abobus/Program.cs
@@ -10,7 +10,8 @@ namespace Abobus
             Console.WriteLine("ок:C");
             Console.WriteLine("не ок ((");
             Console.WriteLine("0к");
-            Console.WriteLine(" НЕ 0К");
+            Console.WriteLine(" НЕ 0К"); 
+            Console.WriteLine("пуп-пип-пап я робот");
         }
     }
 }
